284d7bf2c7e40a419d73234a3eb3c6d427de16d4
ivanprcesur <ivan.s264140@cesurformacion.com>
2022-01-19 12:00:13 +0100

    test unitarios a importar

---
+++ functions.py
@@ -0,0 +1,20 @@
+def sumar(a,b):
+    return a + b
+
+def restar(a,b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
+def potencia(a, b):
+    return (a,b)
+
+def longitud(self):
+    return len(self)
+
+def sumar_numeros(self):
+    return sum(self)
